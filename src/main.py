bbf484f8846b53a72b054882f95219dfdc495f39
Brice Boudehent <brice.boudehent@untienots.com>
2022-05-05 00:11:41 +0200

    writing stats in csv

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from strategies.rsi_adx import RsiAdx
 from strategies.sma_cross import SmaCross
 
 client = Client()
-from_date = datetime.now() - timedelta(days=120)
+from_date = datetime.now() - timedelta(days=20)
 candles = client.get_historical_klines(
     symbol="BTCUSDT",
     interval=Client.KLINE_INTERVAL_30MINUTE,
@@ -22,5 +22,6 @@ chart_data = format_binance_data(candles)
 bt = Backtest(chart_data, RsiAdx, cash=100000, commission=0.002, exclusive_orders=True)
 
 results = bt.run()
-print(results)
+results.to_csv("src/results/stats.csv")
+
 bt.plot(filename="src/results/RsiAdx")
