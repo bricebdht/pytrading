1a08667583e8f9fe1a8612695b670305d20bad8b
Brice Boudehent <brice.boudehent@untienots.com>
2022-05-04 22:27:03 +0200

    adding rsi-adx strategy

--- src/main.py
+++ src/main.py
@@ -5,13 +5,14 @@ from backtesting import Backtest
 from backtesting.test import GOOG
 from binance.client import Client
 from formatters.binance_formatter import format_binance_data
+from strategies.rsi_adx import RsiAdx
 from strategies.sma_cross import SmaCross
 
 client = Client()
-from_date = datetime.now() - timedelta(days=600)
+from_date = datetime.now() - timedelta(days=30)
 candles = client.get_historical_klines(
     symbol="BTCUSDT",
-    interval=Client.KLINE_INTERVAL_1DAY,
+    interval=Client.KLINE_INTERVAL_30MINUTE,
     start_str=from_date.isoformat(),
 )
 
@@ -19,8 +20,8 @@ chart_data = format_binance_data(candles)
 
 # mpf.plot(chart_data.tail(50), type="candle")
 
-bt = Backtest(chart_data, SmaCross, cash=100000, commission=0.002)
+bt = Backtest(chart_data, RsiAdx, cash=100000, commission=0.002)
 
 results = bt.run()
 print(results)
-bt.plot(filename="src/results/SmaCross")
+bt.plot(filename="src/results/RsiAdx")
